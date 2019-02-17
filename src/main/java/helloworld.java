43e2a87eb81fed0dccfc169f2b81296c8eed5971
root <root@localhost.localdomain>
2019-02-17 05:10:30 -0500

    my new file

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -1,8 +1,6 @@
 class helloWorld {
                      public static void main (String args[]) {
                      System.out.println("Hello World");
+                     System.out.println("Hi, hello");
                }
 }
-
-
-hi hi hi   
